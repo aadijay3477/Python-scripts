cdffe5aa84684a7d83e28942ac751dafe1ea6ff7
aaditya jaiswal <aadijay3477@gmail.com>
2024-06-04 05:00:29 +0530

    v1

--- bounty-ground.py
+++ bounty-ground.py
@@ -7,11 +7,14 @@ import random
 MAX_LIMIT_BOUNTY = 100
 PIXEL_OFFSET = 5
 TIME_VARIATION = 0.003
+DRAG_INTERVAL = 5  # Time in seconds between drag operations
 
 IMAGE_PATH = {
     'start-match': r'./Assets/Bounty/Screenshot220.png',
     'matching': r'./Assets/Bounty/Screenshot221.png',
-    'final-screen': r'./Assets/Bounty/Screenshot222.png'
+    'final-screen': r'./Assets/Bounty/Screenshot222.png',
+    'exit': r'./Assets/Bounty/exit.png',
+    'try-again': r'./Assets/Bounty/Tryagain.png',
 }
 
 def random_sleep(base_time):
@@ -28,10 +31,10 @@ def click(x, y, duration=0.0):
 
 def drag(center, duration):
     win = gw.getWindowsWithTitle('Doomsday: Last Survivors')[0]
-    top_margin = win.top + win.height // 4
-    bottom_margin = win.top + 3 * win.height // 4
-    left_margin = win.left + win.width // 4
-    right_margin = win.left + 3 * win.width // 4
+    top_margin = win.top + win.height // 3
+    bottom_margin = win.top + 2 * win.height // 3
+    left_margin = win.left + win.width // 3
+    right_margin = win.left + 2 * win.width // 3
 
     target_x = random.randint(left_margin, right_margin)
     target_y = random.randint(top_margin, bottom_margin)
@@ -41,19 +44,31 @@ def drag(center, duration):
 
     pyautogui.moveTo(center.x + offset_x, center.y + offset_y, duration)
     pyautogui.mouseDown()
+    time.sleep(3)
     pyautogui.moveTo(target_x + offset_x, target_y + offset_y, duration)
     pyautogui.mouseUp()
 
 def for_moving_army_randomly(center, duration):
-    drag(center, duration)
+    last_drag_time = time.time() - DRAG_INTERVAL
+    while True:
+        current_time = time.time()
+        if current_time - last_drag_time >= DRAG_INTERVAL:
+            drag(center, duration)
+            last_drag_time = current_time  # Update last_drag_time after successful drag
+        # Check if 'exit' button appears during drag
+        if pyautogui.locateOnScreen(IMAGE_PATH['exit'], confidence=0.6) is not None:
+            print("Exit button detected during drag. Stopping drag operation.")
+            break
 
-def find_and_click_image(image_key, duration, max_wait=0):
+def find_and_click_image(image_key, duration, max_wait=10):
     image_path = IMAGE_PATH[image_key]
     print(f"Looking for image: {image_key}")
+    # if image_key == 'final-screen': 
+    #     max_wait = 240
     start_time = time.time()
 
     while True:
-        for confidence_threshold in range(10, 3, -1):
+        for confidence_threshold in range(10, 4, -1):
             try:
                 location = pyautogui.locateOnScreen(image_path, confidence=(confidence_threshold / 10.0))
                 if location is not None:
@@ -90,6 +105,11 @@ def activate_window():
     except IndexError:
         print("Window not found!")
 
+def process_step(image_key, click_duration):
+    activate_window()
+    random_sleep(0.2)
+    return find_and_click_image(image_key, click_duration)
+
 def run_macro():
     new_variable = 0
 
@@ -101,21 +121,15 @@ def run_macro():
         new_variable += 1
         random_sleep(0.103)
 
-        activate_window()
-        random_sleep(0.2)
-
-        if find_and_click_image('start-match', 0.1):
+        if process_step('start-match', 0.1):
             random_sleep(0.104)
-
-        activate_window()
-        random_sleep(0.2)
-        
-        if find_and_click_image('matching', 0.15):
+        if process_step('matching', 0.15):
             random_sleep(0.15)
-
-        activate_window()
-        random_sleep(0.2)
-        if find_and_click_image('final-screen', 0.17):
+        if process_step('final-screen', 0.17):
+            random_sleep(0.17)
+        if process_step('exit', 0.17):
+            random_sleep(0.17)
+        if process_step('try-again', 0.17):
             random_sleep(0.17)
 
 if __name__ == "__main__":
